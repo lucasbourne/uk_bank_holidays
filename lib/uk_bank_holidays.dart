13c2f6bbaabd8f35dae001aa1177db17ac8b666e
Lucas <lucas.bourne@outlook.com>
2025-04-21 17:50:26 +0100

    Add holiday checking for today and given date

--- lib/uk_bank_holidays.dart
+++ lib/uk_bank_holidays.dart
@@ -1,6 +1,9 @@
 library;
 
+import 'package:collection/collection.dart';
+import 'package:date_utils/date_utils.dart';
 import 'package:uk_bank_holidays/data/enums/divisions.dart';
+import 'package:uk_bank_holidays/data/models/holiday.dart';
 import 'package:uk_bank_holidays/data/models/holiday_response.dart';
 import 'package:uk_bank_holidays/services/http_client_service.dart';
 
@@ -21,5 +24,34 @@ class UkBankHolidays {
     }
 
     return allResponses;
+  }
+
+  Future<bool> checkHolidayToday({
+    Divisions division = Divisions.englandAndWales,
+  }) => checkHolidayAtDate(DateTime.now(), division: division);
+
+  Future<bool> checkHolidayAtDate(
+    DateTime date, {
+    Divisions division = Divisions.englandAndWales,
+  }) async {
+    final holiday = await getHoliday(date: date, division: division);
+
+    return holiday != null;
+  }
+
+  Future<Holiday?> getHoliday({
+    required DateTime date,
+    Divisions division = Divisions.englandAndWales,
+  }) async {
+    final fetchedHolidays = await getHolidays();
+
+    final holidays =
+        fetchedHolidays
+            .singleWhereOrNull((holiday) => holiday.division == division)
+            ?.holidays;
+
+    return holidays?.firstWhereOrNull(
+      (holiday) => DateUtils.isSameDay(holiday.date!, date),
+    );
   }
 }
